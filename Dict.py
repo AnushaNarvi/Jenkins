aea2b8a7fa2806be7d42bd370f0d3398195b476b
AnushaNarvi <34096168+AnushaNarvi@users.noreply.github.com>
2017-11-29 18:24:50 +0530

    Dict.py

---
+++ Dict.py
@@ -0,0 +1,36 @@
+#/usr/bin/python 
+
+dict={}
+dict['one']="This is one name"
+dict["2"]="This is two"
+
+dict_1={'name':'Anusha','Age':25,'years':3}
+
+print dict
+print dict['one']
+print dict["2"]
+print dict_1.keys()
+print dict_1.values()
+print dict_1['Age']
+
+var=5
+valu=2
+avar="Heell"
+print avar * 2
+print var * 2
+print "divide",var/valu
+print "Remainder",var%valu
+print "add",var+valu
+print "Exponent",var ** 3
+print "Double divison",var//valu
+print var-valu
+
+list = [1,2,3,4]
+print len(list)
+for n in list:
+        print n
+list = ["hello","how","are","you"]
+print list
+for each_num in list:
+    print each_num
+print list[0]
